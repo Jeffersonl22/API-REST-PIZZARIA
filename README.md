9e09d10505de89a00e8b43beb54504e73427b756
jeffersonl22 <jeffersonifnmg@gmail.com>
2021-07-24 13:22:56 -0300

    Upload dos arquivos

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## API
+
+ Essa é uma API REST que permite interagir com um arquivo data.json no servidor. 
+
+
+### VERBOS HTTP
+
+- GET: Receber dados de um Resource.
+- POST: Enviar dados ou informações para serem processados por um Resource.
+- PUT: Atualizar dados de um Resource.
+- DELETE: Deletar um Resource
+
---
+++ server.js
@@ -0,0 +1,59 @@
+/* 
+Descricao :
+	Desenvolvimento da API com as requisições para os verbos HTTP
+Aluno: Jefferson Silva Lopes  
+Data :
+	20/07/2021
+*/
+
+
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+
+app.use(express.json());
+
+app.get("/clients", function(req, res) {
+  res.json(data);
+});
+
+app.get("/clients/:numeroPedido", function(req, res) {
+  const { numeroPedido } = req.params;
+  const client = data.find(cli => cli.numeroPedido == numeroPedido);
+
+  if (!client) return res.status(204).json();
+
+  res.json(client);
+});
+
+app.post("/clients", function(req, res) {
+  const { numeroPedido, descricao, sabor, tamanho, preco, tempoPreparo } = req.body;
+
+  // salvar novo produto
+
+  res.json({ numeroPedido, descricao, sabor, tamanho, preco, tempoPreparo });
+});
+
+app.put("/clients/:numeroPedido", function(req, res) {
+  const { numeroPedido } = req.params;
+  const client = data.find(cli => cli.numeroPedido == numeroPedido);
+
+  if (!client) return res.status(204).json();
+
+  const { sabor } = req.body;
+
+  client.sabor = sabor;
+
+  res.json(client);
+});
+
+app.delete("/clients/:numeroPedido", function(req, res) {
+  const { numeroPedido } = req.params;
+  const clientsFiltered = data.filter(client => client.numeroPedido != numeroPedido);
+
+  res.json(clientsFiltered);
+});
+
+app.listen(3000, function() {
+  console.log("Server is running");
+});
